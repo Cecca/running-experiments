4ae3ed6bb53a7723eee748e41828a63acdaf56a0
Matteo Ceccarello <mceccarello@unibz.it>
2020-06-11 23:56:55 +0200

    Add some comments

--- code/demo.cpp
+++ code/demo.cpp
@@ -5,8 +5,15 @@ int main(int argc, char **argv) {
   // Bring the database schema to the most up to date version
   db_setup();
 
-  // Just a dummy call
-  // record_result("test", 1, "algotest", 1, "params", 128);
+  // Datasets are loaded _by name_, not by filename!
+  // It works as follows: the C++ calls Python and reads its
+  // standard output, which contains the path to the hdf5 file
+  // and the most recent dataset version.
+  // Along the way, if the file or the version didn't exist,
+  // python creates them: each version is stored in a HDF5
+  // group at the top level.
+  //
+  // With this information, C++ reads the dataset from HDF5.
   auto dataset = load("fashion-mnist-784-euclidean");
   auto first = dataset.get(0);
   size_t dim = first.size();
@@ -15,6 +22,9 @@ int main(int argc, char **argv) {
     std::cout << first[i] << " ";
   }
   std::cout << std::endl;
+
+  // Just a dummy call
+  record_result("test", 1, "algotest", 1, "params", 128);
 
   return 0;
 }
