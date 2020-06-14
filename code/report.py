ffd554836cc5c1b3d644f454ed73a6d328de0e62
Martin Aumüller <maau@itu.dk>
2020-06-14 14:06:03 +0200

    adjusted reporting script.

--- code/report.py
+++ code/report.py
@@ -2,7 +2,7 @@ import sqlite3
 import matplotlib.pyplot as plt
 import numpy as np
 
-conn = sqlite3.connect('demo-db.sqlite')
+conn = sqlite3.connect('file:demo-db.sqlite')
 c = conn.cursor()
 
 datasets = [row[0] for row in c.execute('select distinct(dataset) from results;')]
@@ -11,6 +11,6 @@ for ds in datasets:
     print(ds)
     stmt = 'select algorithm, parameters, running_time_ns from results where dataset=?'
     running_times = [row for row in c.execute(stmt, (ds,))]
-    plt.bar(np.arange(len(running_times)), [x[2] // 1000000000 for x in running_times])
-    plt.xticks(np.arange(len(running_times)), [str(x[:2]) for x in running_times])
-    plt.savefig('%s.png' % ds)
+    plt.barh(np.arange(len(running_times)), [x[2] // 1000000000 for x in running_times])
+    plt.yticks(np.arange(len(running_times)), [str(x[:2]) for x in running_times])
+    plt.savefig('%s.png' % ds, bbox_inches='tight')
