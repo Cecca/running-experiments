e28bc9b96d4c3d13eecdc93c45730794215f12e0
Martin Aumüller <maau@itu.dk>
2020-06-18 13:41:45 +0200

    Add filters to reporting script.

--- code/evaluation/report.py
+++ code/evaluation/report.py
@@ -1,16 +1,27 @@
 import sqlite3
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 conn = sqlite3.connect('demo-db.sqlite')
 c = conn.cursor()
 
 datasets = [row[0] for row in c.execute('select distinct(dataset) from recent_results;')]
 
+filter_strings = None
+
+if len(sys.argv) > 1:
+    filter_strings = sys.argv[1:] 
+
 for ds in datasets:
     print(ds)
     plt.figure()
-    stmt = 'select hostname, algorithm, parameters, running_time_ns, recall from recent_results where dataset=?'
+    stmt = 'select hostname, algorithm, parameters, running_time_ns, recall from recent_results where dataset=?' 
+
+    if filter_strings:
+        for s in filter_strings:
+            stmt += " AND parameters LIKE \"%" + s + "%\""
+
     data = [row for row in c.execute(stmt, (ds,))]
     data.sort(key=lambda x: x[3])
     plt.barh(np.arange(len(data)), [x[3] / 1e9 for x in data])
