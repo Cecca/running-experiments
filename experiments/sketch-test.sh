85b5c345f4a3768b154aeb6c23a00af20b14751f
Matteo Ceccarello <mceccarello@unibz.it>
2020-06-19 17:15:15 +0200

    Fix configuration in sketch-test.sh

--- experiments/sketch-test.sh
+++ experiments/sketch-test.sh
@@ -8,7 +8,7 @@ DATASET=glove-100-angular
 for recall in 0.001 0.1 0.2 0.5 0.7 0.9 0.99; do
     for alignment in i16_aligned float_aligned; do
         for method in simple avx2; do
-             ./demo --dataset $DATASET --filter --storage i16_aligned --recall $recall --method $method --experiment-file $FN --seed $SEED
+             ./demo --dataset $DATASET --filter --storage $alignment --recall $recall --method $method --experiment-file $FN --seed $SEED
         done
     done
 done
